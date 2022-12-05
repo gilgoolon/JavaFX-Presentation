a1a7f9db6080b408330176dfc41ffb100c02d840
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2022-12-05 16:51:52 +0200

    Update TransformExampleApp.java

--- Examples/TransformExampleApp.java
+++ Examples/TransformExampleApp.java
@@ -1,10 +1,11 @@
-import javafx.application.Application;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.stage.Stage;
+// This example is taken from the course's book "Java How to Program Early Objects 11th edition"
 
-        import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class TransformExampleApp extends Application {
     @Override
