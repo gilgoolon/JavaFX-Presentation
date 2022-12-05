5c83d0457e1b017c9dbfd6c489adff94f75ac92c
Gil Alpert <87145166+gilgoolon@users.noreply.github.com>
2022-12-05 16:50:54 +0200

    Update AnimationTimerExampleApp.java

--- Examples/AnimationTimerExampleApp.java
+++ Examples/AnimationTimerExampleApp.java
@@ -1,3 +1,5 @@
+// This example is taken from the course's book "Java How to Program Early Objects 11th edition"
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
