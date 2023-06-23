98936b9ed883664d5ceeacc0a60230b573c69726
Gil Alpert <alpertgil@gmail.com>
2023-06-23 09:43:55 +0300

    Examples renamed to src

---
+++ src/AnimationTimerExampleApp.java
@@ -0,0 +1,28 @@
+// This example is taken from the course's book "Java How to Program Early Objects 11th edition"
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AnimationTimerExampleApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("AnimationTimerExampleApp.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.setTitle("Frame-by-Frame Animation Example");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
